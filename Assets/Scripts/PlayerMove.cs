be82e3c0fb99c84d7a08236e6c17c90b5fe26546
Dylan Mines <dylan.e.mines@gmail.com>
2026-03-23 17:45:14 -0600

    movement and particle changes

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ public class PlayerMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int speed;
+    public int dampening;
     public int sensitivity;
 
     [SerializeField]
@@ -14,12 +15,20 @@ public class PlayerMove : MonoBehaviour
 
     private InputAction lookAction;
     InputAction moveAction;
+    InputAction rollLeft;
+    InputAction rollRight;
+
+    private float xrot;
+    private float yrot;
+    private float zrot;
     void Start()
     {
         
         Cursor.lockState = CursorLockMode.Locked;
         moveAction = InputSystem.actions.FindAction("Move");
         lookAction = InputSystem.actions.FindAction("Look");
+        rollLeft = InputSystem.actions.FindAction("RollLeft");
+        rollRight = InputSystem.actions.FindAction("RollRight");
     }
 
     // Update is called once per frame
@@ -29,14 +38,15 @@ public class PlayerMove : MonoBehaviour
 
         if (moveAction.IsPressed() == true)
         {
-            emission.rateOverTime = 30f;
+            emission.rateOverTime = 70f;
             //Debug.Log("moving");
-            Vector3 moveDirection = transform.forward * -(speed / 50);
+            Vector3 moveDirection = transform.forward * -(speed / 50f);
             momentum += moveDirection * Time.deltaTime;
         }
         else
         {
             emission.rateOverTime = 0f;
+            momentum -= momentum * (dampening / 50f) * Time.deltaTime;
             // effects.Pause();
         }
 
@@ -51,8 +61,21 @@ public class PlayerMove : MonoBehaviour
     private void CameraRotation()
     {
         Vector2 lookValue = lookAction.ReadValue<Vector2>();
-        Vector3 rotateVector = new Vector3(-lookValue.y, lookValue.x, 0);
+        int rollValue = 0;
+        if (rollLeft.IsPressed()) { rollValue += 1; }
+        if (rollRight.IsPressed()) { rollValue -= 1; }
+        
+
+        // method 1
+        Vector3 rotateVector = new Vector3(-lookValue.y, lookValue.x, rollValue);
         transform.Rotate(rotateVector * Time.deltaTime * sensitivity);
-            
+
+        // method 2
+        /*yrot += -lookValue.y;
+        xrot += lookValue.x;
+        zrot += rollValue;
+
+        transform.rotation = Quaternion.Euler(new Vector3(yrot, xrot, zrot) * sensitivity/50);*/
+        
     }
 }
